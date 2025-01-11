60005caaa8d5b952e69c229d20ada990416c3e33
Uallace <uallace.macedo@outlook.com>
2025-01-11 12:06:25 -0300

    feat: Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import { ENV_VARS } from './config/envVars.js';
 import router from './routes/index.js';
@@ -5,6 +6,8 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './socket/socket.js';
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +17,11 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(router);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+})
 
 server.listen(ENV_VARS.SERVER_PORT, () => {
   console.log(`> Running as: http://localhost:${ENV_VARS.SERVER_PORT}.`);
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -17,14 +17,12 @@ export const getReceiverSocketId = receiverId => {
 const userSocketMap = {}; // {userId: SocketId}
 
 io.on('connection', (socket) => {
-  console.log(`User connected`, socket.id);
   const userId = socket.handshake.query.userId;
   if(userId != 'undefined') userSocketMap[userId] = socket.id;
 
   io.emit('getOnlineUsers', Object.keys(userSocketMap));
   
   socket.on('disconnect', () => {
-    console.log('User disconnected', socket.id);
     delete userSocketMap[userId];
     io.emit('getOnlineUsers', Object.keys(userSocketMap));
   })
